b5f0b4b723d63112ad7dce65475cc43cf2e8229d
panhariths <panharith.sun@gmail.com>
2024-06-20 11:46:06 +0700

    added ml models

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from typing import Optional
 from pydantic import BaseModel
 import pickle
 import time
-
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 from khmernltk import word_tokenize
 
@@ -16,24 +16,16 @@ from sentiment_model.preprocessing import preprocess
 
 class SentimentResponseSchema(BaseModel):
     sentiments: Optional[list[float]] = []
+    elapsed_time: Optional[float] = 0.0
 class SentimentRequestSchema(BaseModel):
     text: str
 
 router = APIRouter()
 
-@router.post(
-    "/sentiment",
-    response_model=SentimentResponseSchema,
-    # responses={"400": {"model": ExceptionResponseSchema}},
-)
-async def get_sentiment(
-  payload: SentimentRequestSchema
-):
-    start_time = time.time()  
-    
+def process_input(input):
     """ Text input """
 
-    input_text = payload.text
+    input_text = input
 
     """ Text Cleaning """
 
@@ -53,12 +45,9 @@ async def get_sentiment(
     print("Finished embeding text...")
     print("Received embedding of shape: ", embedded.shape)
 
-    """ Sentiment Analysis Model """
+    return embedded
 
-    model = SAModel()
-    prediction = model.predict(embedded)
-
-    print("Finished running model...")
+def show_multiclass_result(prediction, input_text):
 
     prediction_class_index = np.argmax(prediction)
     classes = ['Negative', 'Neutral', 'Positive']
@@ -78,10 +67,35 @@ async def get_sentiment(
 
     sentiment_dict = SentimentResponseSchema()
     sentiment_dict.sentiments = prediction.tolist()[0]
+    
+    return sentiment_dict
+
+@router.post(
+    "/sentiment",
+    response_model=SentimentResponseSchema,
+    # responses={"400": {"model": ExceptionResponseSchema}},
+)
+async def get_sentiment(
+  payload: SentimentRequestSchema
+):
+    start_time = time.time()  
+
+    embedded = process_input(payload.text)
+
+    """ Sentiment Analysis Model """
+
+    model = SAModel()
+    prediction = model.predict(embedded)
+
+    print("Finished running model...")
+
+    sentiment_dict = show_multiclass_result(prediction, payload.text)
 
     end_time = time.time()    # Record the end time
     elapsed_time = end_time - start_time  # Calculate elapsed time
     print(f"Time elapsed: {elapsed_time:.6f} seconds")
+
+    sentiment_dict.elapsed_time = elapsed_time
     
     return sentiment_dict
 
@@ -95,28 +109,8 @@ async def get_sentiment_svm(
   payload: SentimentRequestSchema
 ):
     start_time = time.time()  
-    
-    """ Text input """
 
-    input_text = payload.text
-
-    """ Text Cleaning """
-
-    cleaned_text = preprocess(input_text)
-    print("Finished cleaning text...")
-
-    """ Word tokenization """
-    
-    tokenized_sentence = word_tokenize(cleaned_text)
-    print("Finished tokenizing text...")
-
-    """ Word embedding """
-
-    khmer_bert = BERTKhmerEmbedder()
-    embedded = khmer_bert.extract_word_embeddings([tokenized_sentence])
-
-    print("Finished embeding text...")
-    print("Received embedding of shape: ", embedded.shape)
+    embedded = process_input(payload.text)
 
     """ Sentiment Analysis Model """
 
@@ -128,27 +122,115 @@ async def get_sentiment_svm(
 
     print("Finished running model...")
 
-    prediction_class_index = np.argmax(prediction)
-    classes = ['Negative', 'Neutral', 'Positive']
-
-    # Find the index of the maximum value in the result array
-    overall_sentiment_index = prediction_class_index
-    overall_sentiment = classes[overall_sentiment_index]
-
-    # Print overall sentiment
-    print("Text:", input_text)
-    print("Overall sentiment:", overall_sentiment)
-
-    # Print prediction per class
-    print("\nPrediction per class:")
-    for class_name, probability in zip(classes, prediction[0]):
-        print(f"{class_name}: {probability}")
-
-    sentiment_dict = SentimentResponseSchema()
-    sentiment_dict.sentiments = prediction.tolist()[0]
+    sentiment_dict = show_multiclass_result(prediction, payload.text)
 
     end_time = time.time()    # Record the end time
     elapsed_time = end_time - start_time  # Calculate elapsed time
     print(f"Time elapsed: {elapsed_time:.6f} seconds")
+
+    sentiment_dict.elapsed_time = elapsed_time
+    
+    return sentiment_dict
+
+
+@router.post(
+    "/sentiment-knn",
+    response_model=SentimentResponseSchema,
+    # responses={"400": {"model": ExceptionResponseSchema}},
+)
+async def get_sentiment_knn(
+  payload: SentimentRequestSchema
+):
+    start_time = time.time()  
+
+    embedded = process_input(payload.text)
+
+    """ Sentiment Analysis Model """
+
+    with open('/Users/panharithsun/Documents/fyp_demo/FastAPI-template/sentiment_model/sentiment_knn.pkl', 'rb') as f:
+        model = pickle.load(f)
+    prediction = model.predict_proba(embedded)
+
+    print(prediction)
+
+    print("Finished running model...")
+
+    sentiment_dict = show_multiclass_result(prediction, payload.text)
+
+    end_time = time.time()    # Record the end time
+    elapsed_time = end_time - start_time  # Calculate elapsed time
+    print(f"Time elapsed: {elapsed_time:.6f} seconds")
+    
+    sentiment_dict.elapsed_time = elapsed_time
+    
+    return sentiment_dict
+
+
+@router.post(
+    "/sentiment-nb",
+    response_model=SentimentResponseSchema,
+    # responses={"400": {"model": ExceptionResponseSchema}},
+)
+async def get_sentiment_nb(
+  payload: SentimentRequestSchema
+):
+    start_time = time.time()  
+
+    embedded = process_input(payload.text)
+
+    scaler = MinMaxScaler()
+    scaled_embedded = scaler.fit_transform(embedded)
+
+
+    """ Sentiment Analysis Model """
+
+    with open('/Users/panharithsun/Documents/fyp_demo/FastAPI-template/sentiment_model/sentiment_nb.pkl', 'rb') as f:
+        model = pickle.load(f)
+    prediction = model.predict_proba(scaled_embedded)
+
+    print(prediction)
+
+    print("Finished running model...")
+
+    sentiment_dict = show_multiclass_result(prediction, payload.text)
+
+    end_time = time.time()    # Record the end time
+    elapsed_time = end_time - start_time  # Calculate elapsed time
+    print(f"Time elapsed: {elapsed_time:.6f} seconds")
+    
+    sentiment_dict.elapsed_time = elapsed_time
+    
+    return sentiment_dict
+
+
+@router.post(
+    "/sentiment-rf",
+    response_model=SentimentResponseSchema,
+    # responses={"400": {"model": ExceptionResponseSchema}},
+)
+async def get_sentiment_rf(
+  payload: SentimentRequestSchema
+):
+    start_time = time.time()  
+
+    embedded = process_input(payload.text)
+
+    """ Sentiment Analysis Model """
+
+    with open('/Users/panharithsun/Documents/fyp_demo/FastAPI-template/sentiment_model/sentiment_rf.pkl', 'rb') as f:
+        model = pickle.load(f)
+    prediction = model.predict_proba(embedded)
+
+    print(prediction)
+
+    print("Finished running model...")
+
+    sentiment_dict = show_multiclass_result(prediction, payload.text)
+
+    end_time = time.time()    # Record the end time
+    elapsed_time = end_time - start_time  # Calculate elapsed time
+    print(f"Time elapsed: {elapsed_time:.6f} seconds")
+    
+    sentiment_dict.elapsed_time = elapsed_time
     
     return sentiment_dict
